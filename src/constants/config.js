364fa743331fb9af8a150e448b31faac7280b86f
Eric Stevens <ericstevens26101@gmail.com>
2025-04-01 17:43:15 -0700

    match  url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://aksqkozx57.execute-api.us-west-2.amazonaws.com/prod'; // Will update after deployment
-export const REGION = 'us-west-2'; // Choose your AWS region
-export const COGNITO_USER_POOL_ID = 'us-west-2_Fua1tqt5r';
-export const COGNITO_CLIENT_ID = '32vn4dh03rov7ijgbk67br2pk1';
+export const API_BASE_URL = 'https://t9thkhqjde.execute-api.us-west-2.amazonaws.com/prod'; // Updated after TensorTours rebranding
+export const REGION = 'us-west-2'; // AWS region
+export const COGNITO_USER_POOL_ID = 'us-west-2_XuSMT3Ymj';
+export const COGNITO_CLIENT_ID = '4a20alvb40fc3nefolobv6juqk';
